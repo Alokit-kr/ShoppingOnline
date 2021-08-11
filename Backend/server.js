d7ed3b16a217b9e62d754726d95b826ea894f898
Alokit Kumar <64124927+Alokit-kr@users.noreply.github.com>
2021-08-11 18:02:54 +0530

    Running server and client by same command

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const dotenv=require('dotenv')
+const products=require('./Data/products');
+dotenv.config()
+const app=express();
+app.get('/',function(req,res){
+  res.send("API is running....");
+});
+app.get('/api/products',function(req,res){
+    res.json(products);
+});
+app.get('/api/products/:id',function(req,res){
+    const product=products.find((p)=>p._id===req.params.id)
+    res.json(product);
+});
+const PORT=process.env.PORT||5000
+app.listen(PORT,function(){
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
